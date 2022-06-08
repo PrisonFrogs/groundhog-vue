d4955f73d97cb8160caed4090432e3702dd51ffc
uooobarry <383435763@qq.com>
2022-06-08 14:34:10 +0800

    Add posts layout

---
+++ src/components/layouts/AppBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <v-app-bar dense>
+      <v-tabs v-model="selectedTab" centered>
+        <v-tab v-for="(tab, i) in tabs" :key="i" :disabled="tab.disabled">{{ tab.text }}</v-tab>
+      </v-tabs>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+  data() {
+    return {
+      tabs: [{ text: '关注', disabled: true }, { text: '发现' }, { text: '地区', disabled: true }],
+      selectedTab: 1,
+    };
+  },
+};
+</script>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Posts />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Posts from '@/components/posts/index.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    Posts,
+  },
+};
+</script>
