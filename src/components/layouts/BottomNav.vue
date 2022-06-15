65645021286f83d21b8fe544b8f5bc3453bba8e8
s3713572 <asrangetoop@gmail.com>
2022-06-15 11:46:55 +1000

    add bottom bar

---
+++ src/components/layouts/BottomNav.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-bottom-navigation
+    :value="value"
+    color="primary"
+    class="bottom-bar"
+  >
+    <v-btn>
+      <span>Recents</span>
+
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<style scoped>
+.bottom-bar{
+  position: fixed;
+  bottom: 0;
+}
+</style>
